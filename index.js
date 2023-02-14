f54904fefe636236e348ce4e6ea795324d762149
Vladimir RM <shvdrummer@gmail.com>
2023-02-14 09:58:46 +0200

    add reducer

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // sum all arr
 
 const array = [1, 2, 3, 4, 5];
-let sum = 0;
+// let sum = 0;
 
 for (let i = 0; i < array.length; i++) {
   sum += array[i];
 }
+
+let sum = array.reduce((ac,cur) => ac + cur ) 
 
 console.log(sum);
