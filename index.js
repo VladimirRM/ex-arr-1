dc9a3a44bf15f7cb34f27979108a92e3e33df90c
Vladimir RM <shvdrummer@gmail.com>
2023-02-14 10:29:41 +0200

    sum arr done

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-// sum all arr
+// let arr = [1,2,3,4,5,6]
 
-const array = [1, 2, 3, 4, 5];
-// let sum = 0;
+// let sum = 0
 
-for (let i = 0; i < array.length; i++) {
-  sum += array[i];
-}
+// for (let i = 0; i<arr.length; i++){
+//       sum += arr[i]
+// }
+// console.log(sum);
+// let sum = arr.reduce((acc,cur)=> acc + cur)
 
-let sum = array.reduce((ac,cur) => ac + cur ) 
-
-console.log(sum);
+// console.log(sum);
