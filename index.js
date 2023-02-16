0f0352af3ac293dd4ee6140e7020c66a0137476d
Vladimir RM <shvdrummer@gmail.com>
2023-02-16 14:54:33 +0200

    task condition

--- index.js
+++ index.js
@@ -1,24 +1,6 @@
-// let users = [
-//   {
-//     name:' Ivan',
-//     age: 35,
-//   },
-//   {
-//     name:' petro ',
-//     age: 38,
-//   },
-//   {
-//     name:' Ivan',
-//     age: 24,
-//   },
-// ]
+// sum  for reduce 
+// max  for reduce Math
+// sort  number item 
 
-// users.sort((x1,x2)=> x1.age-x2.age)
 
-// console.log(users);
 
-let arr = [1, 4, 3, 2, 7, 6, 5];
-
-arr.sort((x1, x2) => x1 - x2);
-
-console.log(arr);
