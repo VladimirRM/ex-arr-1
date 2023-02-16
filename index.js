28c6ee0bcdbf763924fe6392db9531ca2a39976f
Vladimir RM <shvdrummer@gmail.com>
2023-02-16 10:21:19 +0200

    sum number with reduce

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-let arr = [1, 2, 3, 4, 5];
+let arr = [1,2,3,4,5,6]
 
-let sum = 0;
 
-for (let i = 0; i < arr.length; i++) {
-  sum += arr[i];
-}
+let sum = arr.reduce((acc,next)=>acc + next)
+
 console.log(sum);
