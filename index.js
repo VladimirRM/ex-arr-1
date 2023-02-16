f9ac34afd55d49a3bbd68ccd3784d8825f323733
Vladimir RM <shvdrummer@gmail.com>
2023-02-16 14:51:43 +0200

    sort and revers arr

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-let users = [
-  {
-    name:' Ivan',
-    age: 35,
-  },
-  {
-    name:' petro ',
-    age: 38,
-  },
-  {
-    name:' Ivan',
-    age: 24,
-  },
-]
+// let users = [
+//   {
+//     name:' Ivan',
+//     age: 35,
+//   },
+//   {
+//     name:' petro ',
+//     age: 38,
+//   },
+//   {
+//     name:' Ivan',
+//     age: 24,
+//   },
+// ]
 
-users.sort((x1,x2)=> x1.age-x2.age)
+// users.sort((x1,x2)=> x1.age-x2.age)
 
-console.log(users);
+// console.log(users);
+
+let arr = [1, 4, 3, 2, 7, 6, 5];
+
+arr.sort((x1, x2) => x1 - x2);
+
+console.log(arr);
