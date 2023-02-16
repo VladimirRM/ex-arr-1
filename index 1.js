2ef078123d9eae5bf2c7efb23363024a1ea81ffb
Vladimir RM <shvdrummer@gmail.com>
2023-02-16 18:05:48 +0200

    max Math for done

--- index 1.js
+++ index 1.js
@@ -24,9 +24,6 @@
 //     }
 // }
 
-
-// let arr = [1, 2, 3, 4, 5, 6,8];
-
 // let max = arr[0]
 
 // for  (let i=1; i<arr.length;i++){
--- index.js
+++ index.js
@@ -2,15 +2,13 @@
 // max  for reduce Math
 // sort  number item
 
-let arr =[1,2,3,4,5,8]
+let arr =[1,2,3,4,6,13]
 
 
 let max = arr[0]
 
 for(let i=0; i<arr.length;i++){
-  if(max < arr[i]){
-    max = arr[i]
-  }
+    max = Math.max(arr[i],max)
 }
 
 console.log(max);
