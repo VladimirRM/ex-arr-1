5be914203ae46cb65458c277da08337c0fde1524
Vladimir RM <shvdrummer@gmail.com>
2023-02-16 17:59:32 +0200

    sum reduce done

--- index.js
+++ index.js
@@ -2,12 +2,10 @@
 // max  for reduce Math
 // sort  number item
 
-let arr =[1,2,3,4,5,6]
+let arr =[1,2,3,4,5,]
 
-let sum = arr[0]
-
-for (let i=0; i<arr.length;i++){
-        sum +=arr[i]
-}
+let sum = arr.reduce((acc,next)=>acc + next)
 
 console.log(sum);
+
+
