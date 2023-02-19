d54de34705adcd26fb88ebd37df0488d8dc79df6
Vladimir RM <shvdrummer@gmail.com>
2023-02-19 15:36:57 +0200

    sort num done

--- index.js
+++ index.js
@@ -4,8 +4,10 @@
 // reverse for, map;
 // false arr for, filter
 
-let arr = [1, 2, 3, 4, 5, 6, 7];
 
-let max = arr.reduce((acc, next) => Math.max(acc, next));
+let arr = [1,3,6,9,3,6,8]
 
-console.log(max);
+arr.sort((x1,x2)=>(x1 - x2))
+
+
+console.log(arr);
