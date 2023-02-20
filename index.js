3e62ee8733e6b6a041375e675daeac26bc74e16e
Vladimir RM <shvdrummer@gmail.com>
2023-02-20 09:21:59 +0200

    max for done

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-// sum  for reduce;
-// max  for reduce Math reduce-Math;
+// sum  for, reduce;
+// max  for, reduce, Math reduce-Math;
 // sort  number, item, localeCompare;
 // reverse for, map;
 // false arr for, filter
 
-let arr = [1, 2, 3, 5, 6, 7];
+let arr = [1, 2, 3, 5, 6, 7, 8];
 
-let sum = arr.reduce((acc, next) => acc + next);
+let max = arr[0];
 
-console.log(sum);
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] > max) {
+    max = arr[i];
+  }
+}
+console.log(max);
