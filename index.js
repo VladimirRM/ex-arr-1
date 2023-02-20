cbd2ee64293bd06de64c4f519194d848540ea17c
Vladimir RM <shvdrummer@gmail.com>
2023-02-20 09:19:14 +0200

    sum reduce done

--- index.js
+++ index.js
@@ -4,12 +4,8 @@
 // reverse for, map;
 // false arr for, filter
 
-let arr = [1, 2, 3, 5, 6, 7, 8];
+let arr = [1, 2, 3, 5, 6, 7];
 
-let sum = 0;
-
-for (let i = 0; i < arr.length; i++) {
-  sum += arr[i];
-}
+let sum = arr.reduce((acc, next) => acc + next);
 
 console.log(sum);
