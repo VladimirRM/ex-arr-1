4a7bb3d7c8846fd07fde2a6f59970b3e79427ca4
Vladimir RM <shvdrummer@gmail.com>
2023-02-23 10:17:57 +0200

    start app

--- index.js
+++ index.js
@@ -4,12 +4,4 @@
 // reverse for, map;
 // false arr for, filter
 
-let arr = [1, 2, 8, 3, 5, 6, ];
 
-let max = arr[0]
-
-
-for( let i =0; i <arr.length; i++){
-    max = Math.max(arr[i], max)
-}
-console.log(max);
