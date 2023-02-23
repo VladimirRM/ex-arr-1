8282f42b1373950eb6b99e625b310ab8e9cb3954
Vladimir RM <shvdrummer@gmail.com>
2023-02-23 10:47:12 +0200

    notFalsy Boolean done

--- index.js
+++ index.js
@@ -4,8 +4,10 @@
 // reverse for, map;
 // false arr for, filter
 
-let arr = [1, 2, 3, 4, 5, 6, 7];
+let arr = [1, NaN, "Ivan", false, 31, "6", -7];
 
-let rev = arr.map((e, i, arr) => arr[arr.length - i - 1]);
+let notFalsy = [];
 
-console.log(rev);
+notFalsy = arr.filter(Boolean);
+
+console.log(notFalsy);
