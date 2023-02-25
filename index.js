dd8c1072de260e947f6307675b08bab91f11821a
Vladimir RM <shvdrummer@gmail.com>
2023-02-25 08:57:59 +0200

    max number for

--- index.js
+++ index.js
@@ -8,9 +8,12 @@
 
 let arr = [1, 2, 3, 4, 5, 6, 7];
 
-let sum = 0;
+let max = arr[0];
 
 for (let i = 0; i < arr.length; i++) {
-  sum += arr[i];
+  if (max < arr[i]) {
+    max = arr[i];
+  }
 }
-console.log(sum);
+
+console.log(max);
