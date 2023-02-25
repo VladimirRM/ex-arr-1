5c4a55112598391c848f0c5c637762355f5cc743
Vladimir RM <shvdrummer@gmail.com>
2023-02-25 09:25:47 +0200

    not falsy filter done

--- index.js
+++ index.js
@@ -4,25 +4,9 @@
 // reverse for, map;
 // false arr for, filter
 
-// let arr = [1, NaN, "Ivan", false, 31, "6", -7];
+let arr = [1, NaN, "Ivan", false, 31, "6", -7];
 
-let user = [
-  {
-    name: "Ivan",
-    age: 21,
-  },
-  {
-    name: "Anna",
-    age: 35,
-  },
-  {
-    name: "Yanina",
-    age: 26,
-  },
-];
+let notFalsy = arr.filter(Boolean)
 
-user.sort((x1, x2) => {
- return x1.name.localeCompare(x2.name);
-});
 
-console.log(user);
+console.log(notFalsy);
