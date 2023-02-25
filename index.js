d37c7b8ad5ba5718ac38ae510ffb8919fb3a0773
Vladimir RM <shvdrummer@gmail.com>
2023-02-25 08:59:20 +0200

    reduce math done

--- index.js
+++ index.js
@@ -6,14 +6,8 @@
 
 // let arr = [1, NaN, "Ivan", false, 31, "6", -7];
 
-let arr = [1, 2, 3, 4, 5, 6, 7];
+let arr = [1, 2, 3, 4, 5, 6, 7,8];
 
-let max = arr[0];
-
-for (let i = 0; i < arr.length; i++) {
-  if (max < arr[i]) {
-    max = arr[i];
-  }
-}
+let max = arr.reduce((acc, next) => Math.max(acc, next));
 
 console.log(max);
