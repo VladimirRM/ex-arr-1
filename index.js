2ce90a530533c120e7a0a4dd9775559a62ca41f8
Vladimir RM <shvdrummer@gmail.com>
2023-02-26 09:28:23 +0200

    max for Math done

--- index.js
+++ index.js
@@ -4,8 +4,11 @@
 // reverse for, map;
 // false arr for, filter
 
-let arr = [1, 2, 3, 4, 5, 6, 7, ];
+let arr = [1, 2, 3, 4, 5, 6, 7];
 
-let max = arr.reduce((acc, next) => Math.max(acc, next));
+let max = arr[0];
 
+for (let i = 0; i < arr.length; i++) {
+  max = Math.max(arr[i], max);
+}
 console.log(max);
