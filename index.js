7be9d20d58e3a76dfc2fa50230e449ff14154d5b
Vladimir RM <shvdrummer@gmail.com>
2023-02-28 19:02:59 +0200

    sort localeCompare done

--- index.js
+++ index.js
@@ -6,23 +6,23 @@
 
 // const arr = [7, 0, "hello", 5, false, NaN];
 
- user =[
-    {
-        name:'Ivan',
-        age: 40
-    },
-    {
-        name:'Yana',
-        age: 15
-    },
-    {
-        name:'Alyosha',
-        age: 12
-    },
-]
+user = [
+  {
+    name: "Ivan",
+    age: 40,
+  },
+  {
+    name: "Yana",
+    age: 15,
+  },
+  {
+    name: "Alyosha",
+    age: 12,
+  },
+];
 
-user.sort((x1,x2)=> x1.age - x2.age)
-
-
+user.sort((x1, x2) => {
+  return x1.name.localeCompare(x2.name);
+});
 
 console.log(user);
