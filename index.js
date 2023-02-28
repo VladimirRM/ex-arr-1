9fda00579da4f2718972af8453b9504d3774fc68
Vladimir RM <shvdrummer@gmail.com>
2023-02-28 18:44:25 +0200

    max for done

--- index.js
+++ index.js
@@ -8,6 +8,12 @@
 
 let arr = [1, 2, 3, 4, 5, 6];
 
-let sum = arr.reduce((acc, next) => acc + next);
+let max = arr[0];
 
-console.log(sum);
+for (let i = 0; i < arr.length; i++) {
+  if (max < arr[i]) {
+    max = arr[i];
+  }
+}
+
+console.log(max);
