8593d1ae7d6aee20f9a693f16ae1fd5c8befdcaf
Vladimir RM <shvdrummer@gmail.com>
2023-03-01 10:29:50 +0200

    sort age done

--- index.js
+++ index.js
@@ -6,8 +6,21 @@
 
 // const arr = [7, 0, "hello", 5, false, NaN];
 
-let arr = [2, 4, 7, 5, 4];
+let user = [
+  {
+    user: "ivan",
+    age: 42,
+  },
+  {
+    user: "Yana",
+    age: 43,
+  },
+  {
+    user: "Anna",
+    age: 23,
+  },
+];
 
-arr.sort((x1, x2) => x1 - x2);
+user.sort((x1, x2) => x1.age - x2.age);
 
-console.log(arr);
+console.log(user);
