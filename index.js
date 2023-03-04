39cb633a3e34612ae24a6b9b5807910ae29e8866
Vladimir RM <shvdrummer@gmail.com>
2023-03-04 09:26:22 +0200

    for sum done

--- index.js
+++ index.js
@@ -6,4 +6,12 @@
 
 // const arr = [7, 0, "hello", 5, false, NaN];
 
+let arr = [1, 2, 3, 4, 5, 6, 7];
 
+let sum = 0;
+
+for (let i = 0; i < arr.length; i++) {
+  sum += arr[i];
+}
+
+console.log(sum);
