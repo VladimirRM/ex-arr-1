340e2853c67053b464bd7f35479b5ebaf01a0d50
Vladimir RM <shvdrummer@gmail.com>
2023-03-05 16:58:43 +0200

    max Math & for done

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ let arr = [1, 2, 3, 4, 5, 6, 7];
 let max = arr[0];
 
 for (let i = 0; i < arr.length; i++) {
-  if (max < arr[i]) {
-    max = arr[i];
-  }
+  max = Math.max(arr[i], max);
 }
 
 console.log(max);
