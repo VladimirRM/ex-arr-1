5bdd771a9f53084b1fa20574550e0950108be01f
Vladimir RM <shvdrummer@gmail.com>
2023-03-08 08:20:47 +0200

    save template

--- index.js
+++ index.js
@@ -3,9 +3,4 @@
 // sort  number, item, localeCompare;
 // reverse for, map;
 // false arr for, filter
-
-const arr = [7, 0, "hello", 5, false, NaN];
-
-let notFalsy = arr.filter(Boolean);
-
-console.log(notFalsy);
+// const arr = [7, 0, "hello", 5, false, NaN];
