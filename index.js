4b82111e62d4bf4dee226ef66908ec523440f770
Vladimir RM <shvdrummer@gmail.com>
2023-03-09 10:17:12 +0200

    max number with for done

--- index.js
+++ index.js
@@ -11,13 +11,11 @@
 // const arr = [7, 0, "hello", 5, false, NaN];
 
 let arr = [1, 2, 3, 4, 5, 6, 7];
+let max = arr[0];
 
-
-for ( let i =0; i<arr.length;i++){
-    if( arr[i] > 0 && arr[i] <5){
-        sum = arr[i]
-        console.log(sum)
-        ;
-    }
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] > max) {
+    max = arr[i];
+  }
 }
-
+console.log(max);
