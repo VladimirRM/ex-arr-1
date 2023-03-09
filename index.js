125c3e0f8494f5de596496264f26945fcf04b420
Vladimir RM <shvdrummer@gmail.com>
2023-03-09 09:29:51 +0200

    add html logic

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 // sum  for, reduce;
-// average sum for
+// average sum for;
+// show number between condition
+// create 20 random number
 // max  for, reduce, Math reduce-Math;
 // sort  number, item, localeCompare;
 // reverse for, map;
 // false arr for, filter
 // const arr = [7, 0, "hello", 5, false, NaN];
 
-let arr = []
+// let arr = []
 
-for( let i =0; i < 20; i++){
-    arr.push(Math.round(Math.random()* 20) +1)
-}
+// for( let i =0; i < 20; i++){
+//     arr.push(Math.round(Math.random()* 20) +1)
+// }
 
-console.log(arr);
+// console.log(arr);
+const textOne = document.querySelector(".out");
+textOne.textContent = 'hi';
+
+// const textTwo = document.createElement("div");
+
+// textTwo.innerHTML = `Second`;
+// textOne = innerHTML;
+
+// console.log(textOne);
+// console.log(textTwo);
