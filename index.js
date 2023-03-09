ffb2943775d553b16dac9bca3fad8557148aa726
Vladimir RM <shvdrummer@gmail.com>
2023-03-09 12:47:06 +0200

    get number in array done

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ let arr = [1, 2, 3, 4, 5, 6, 7];
 let sum = 0;
 
 for (let i = 0; i < arr.length; i++) {
-  sum += arr[i];
-  result = sum / arr.length;
+  if (arr[i] === 5) {
+    sum = arr[i]
+    console.log(sum);
+  }
 }
-
-console.log(result);
