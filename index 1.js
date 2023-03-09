90cade3db3060c3da72c571b0b5c1f59b49ccdf9
Vladimir RM <shvdrummer@gmail.com>
2023-03-09 10:15:35 +0200

    show num between num done

--- index 1.js
+++ index 1.js
@@ -181,7 +181,7 @@
 // console.log(sum);
 
 
-//////// show number between condition ///////
+//////// get number between number ///////
 // let arr = [1, 2, 3, 5, 6, 7, 8, 9];
 
 // let num = 0;
--- index.js
+++ index.js
@@ -12,10 +12,12 @@
 
 let arr = [1, 2, 3, 4, 5, 6, 7];
 
-let sum = 0;
 
-
-
+for ( let i =0; i<arr.length;i++){
+    if( arr[i] > 0 && arr[i] <5){
+        sum = arr[i]
+        console.log(sum)
+        ;
+    }
 }
-
 
