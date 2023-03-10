b7d588a9433c80b00347e169c06c4961b4ed7224
Vladimir RM <shvdrummer@gmail.com>
2023-03-10 10:07:53 +0200

    calc square done

--- index.js
+++ index.js
@@ -12,16 +12,16 @@
 
 // const arr = [7, 0, "hello", 5, false, NaN];
 
-// let arr = [1, 2, 2, 2, 3, 4, 5, 5, 5, 5, 6];
+let arr = [1, 2, 2, 2, 3, 4, 5, 5, 5, 5, 6];
 
 // let result = [];
 
 // let arr = [1, 2, 2, -3, 4, 4, -4, 5, 5, -5, 6, -7];
 
-let res = []
+let result = 0
 
-for (let i = 0; i = 20; i++) {
-   res.push(Math.round(Math.random() * 20) + 1)
-  }
+for( let i = 0; i<arr.length; i++){
+     result += arr[i] * arr[i]
+}
 
-console.log(res);
+console.log(result);
