8326b197b7f164e4b964c16c25d6bcafd401bb87
Vladimir RM <shvdrummer@gmail.com>
2023-03-10 09:41:54 +0200

    max for done

--- index.js
+++ index.js
@@ -18,19 +18,11 @@
 
 let arr = [1, 2, 2, -3, 4, 4, -4, 5, 5, -5, 6, -7];
 
-let sum = 0 
+let max = arr[0];
 
-for( let i = 0; i<arr.length;i++){
-    sum += arr[i]
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] > max) {
+    max = arr[i];
+  }
 }
-console.log(sum);
-
-
-
-
-
-
-
-
-
-
+console.log(max);
