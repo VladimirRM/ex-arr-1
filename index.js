cf39fe10689b11b6340112cdf618c44b9945e17d
Vladimir RM <shvdrummer@gmail.com>
2023-03-11 08:09:34 +0200

    average num done

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ let sum = 0;
 
 for (let i = 0; i < arr.length; i++) {
   sum += arr[i];
+  result = sum / arr.length;
 }
-console.log(sum);
+console.log(result);
 
 // for( let i = 0; i<arr.length; i++){}
