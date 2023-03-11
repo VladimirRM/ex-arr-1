4d069c4a41c07ca9cf33cb86248d713b5b418412
Vladimir RM <shvdrummer@gmail.com>
2023-03-11 08:35:51 +0200

    boolean select done

--- index.js
+++ index.js
@@ -10,12 +10,18 @@
 // reverse num, for, map;
 // false arr for, filter
 
-// const arr = [7, 0, "hello", 5, false, NaN];
+const arr = [7, 0, "hello", 5, false, NaN];
 
-let arr = [1, 2, -2, 2, 3, -4, 5, -5, 5, 5, 6, 7];
+// let arr = [1, 2, -2, 2, 3, -4, 5, -5, 5, 5, 6, 7];
 
-let sum = arr.map((item, i, arr) => arr[arr.length - i - 1]);
+result = [];
 
-console.log(sum);
+for (let i = 0; i < arr.length; i++) {
+  if (!!arr[i]) {
+    result.push(arr[i]);
+  }
+}
+
+console.log(result);
 
 // for( let i = 0; i<arr.length; i++){}
