5816ad5cbd11b15135d451d462250ea78052c1be
Vladimir RM <shvdrummer@gmail.com>
2023-03-12 08:22:20 +0200

    sum for done

--- index.js
+++ index.js
@@ -10,18 +10,14 @@
 // reverse num, for, map;
 // false arr for, filter
 
-const arr = [7, 0, "hello", 5, false, NaN];
+// const arr = [7, 0, "hello", 5, false, NaN];
 
-// let arr = [1, 2, -2, 2, 3, -4, 5, -5, 5, 5, 6, 7];
+let arr = [1, 2, -2, 2, 3, -4, 5, -5, 5, 5, 6, 7];
 
-result = [];
-
+let sum = 0;
 for (let i = 0; i < arr.length; i++) {
-  if (!!arr[i]) {
-    result.push(arr[i]);
-  }
+  sum += arr[i];
 }
-
-console.log(result);
+console.log(sum);
 
 // for( let i = 0; i<arr.length; i++){}
