aa4e49f16eeeeb4955815ef2d5e5ec69f9ac17cd
Vladimir RM <shvdrummer@gmail.com>
2023-03-12 08:32:28 +0200

    max num with for and Math done

--- index.js
+++ index.js
@@ -17,9 +17,7 @@ let arr = [1, 2, -2, 2, 3, -4, 5, -5, 5, 5, 6, 7];
 let sum = 0;
 
 for (let i = 0; i < arr.length; i++) {
-  if (arr[i] > sum) {
-    sum = arr[i];
-  }
+    sum = Math.max(arr[i],sum)
 }
 console.log(sum);
 
