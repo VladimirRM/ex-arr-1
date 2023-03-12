548df1534236f091f600665c16fc5ba7bdba36f5
Vladimir RM <shvdrummer@gmail.com>
2023-03-12 08:26:26 +0200

    find number done

--- index.js
+++ index.js
@@ -17,10 +17,11 @@ let arr = [1, 2, -2, 2, 3, -4, 5, -5, 5, 5, 6, 7];
 let sum = 0;
 
 for (let i = 0; i < arr.length; i++) {
-  sum += arr[i];
-  result = sum / arr.length;
+  if (arr[i] === 3) {
+    sum = arr[i];
+  }
 }
 
-console.log(result);
+console.log(sum);
 
 // for( let i = 0; i<arr.length; i++){}
