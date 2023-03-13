1497ac429021d8d2235521578c77132550772533
Vladimir RM <shvdrummer@gmail.com>
2023-03-13 08:49:47 +0200

    sum for done

--- index.js
+++ index.js
@@ -1,29 +1,25 @@
-// sum  for, reduce;
-// average sum for;
-// find number in array
-// delete duplicate in array
-// show number between range
-// max  for, reduce, Math reduce-Math;
-// create 20 random number
-// calc square all number
-// sort  number, item, localeCompare;
-// reverse num, for, map;
-// false arr for, filter
+// 1 sum  for, reduce;
+// 2 average sum for;
+// 3 find number in array
+// 4 delete duplicate in array
+// 5 show number between range
+// 6 max  for, reduce, Math reduce-Math;
+// 7 create 20 random number
+// 8 calc square all number
+// 9 sort  number, item, localeCompare;
+// 10 reverse num, for, map;
+// 11 false arr for, filter
 
-const arr = [7, 0, "hello", 5, false, NaN];
+// const arr = [7, 0, "hello", 5, false, NaN];
 
 // let arr = [1, 2, -2, 2, 3, -4, 5, -5, 5, 5, 6, 7];
 
-// let arr = [1, 2, 3, 4, 5, 6, 7];
-
-let res = [];
+let arr = [1, 2, 3, 4, 5, 6, 7];
+let sum = 0;
 
 for (let i = 0; i < arr.length; i++) {
-  if (!!arr[i]) {
-    res.push(arr[i]);
-  }
+  sum += arr[i];
 }
-
-console.log(res);
+console.log(sum);
 
 // for( let i = 0; i<arr.length; i++){}
