f044c985d075faa56dd369bcf9f786d083ca40cc
Vladimir RM <shvdrummer@gmail.com>
2023-03-13 09:00:57 +0200

    find number in array done

--- index.js
+++ index.js
@@ -18,9 +18,10 @@ let arr = [1, 2, 3, 4, 5, 6, 7];
 let sum = 0;
 
 for (let i = 0; i < arr.length; i++) {
-  sum += arr[i];
-  result = sum / arr.length;
+  if (arr[i] === 5) {
+
+    console.log(arr[i]);
+  }
 }
-console.log(result);
 
 // for( let i = 0; i<arr.length; i++){}
