bbdc3c3c7b9f2ecc26f2094e89e5acab3433a122
Vladimir RM <shvdrummer@gmail.com>
2023-03-14 09:42:40 +0200

    max number done

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ let arr = [1, 2, -2, 2, 3, -4, 5, -5, 5, 5, 6, 7];
 let sum = [];
 
 for (let i = 0; i < arr.length; i++) {
-  if (arr[i] > 0 && arr[i] < 5) {
-    sum.push(arr[i]);
+  if (arr[i] > sum) {
+    sum = arr[i];
   }
 }
 console.log(sum);
