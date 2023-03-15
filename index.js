ebb3091b653d8c990ab5a342ec774c5a1dd01ee5
Vladimir RM <shvdrummer@gmail.com>
2023-03-15 07:50:37 +0200

    average for done

--- index.js
+++ index.js
@@ -21,9 +21,10 @@ let arr = [1, 2, 3, 4, 5, 6, 7];
 let sum = 0;
 
 for (let i = 0; i < arr.length; i++) {
-      sum += arr[i]
+  sum += arr[i];
+  result = sum / arr.length;
 }
 
-console.log(sum);
+console.log(result);
 
 // for( let i = 0; i<arr.length; i++){}
