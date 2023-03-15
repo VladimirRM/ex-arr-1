35660fafc8659e55f453b0f46fdc2090b2780e06
Vladimir RM <shvdrummer@gmail.com>
2023-03-15 08:03:59 +0200

    calc square all num done

--- index.js
+++ index.js
@@ -18,11 +18,11 @@ let arr = [1, 2, 3, 4, 5, 6, 7];
 
 // let arr = [];
 
-let sum = arr.reduce((a, b) => Math.max(a, b));
+let sum = 0;
 
-// for (let i = 0; i < arr.length; i++) {
-//   sum = Math.max(arr[i], sum);
-// }
+for (let i = 0; i < arr.length; i++) {
+  sum += arr[i] * arr[i];
+}
 
 console.log(sum);
 
