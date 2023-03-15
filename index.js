6009e678b68ea4568efc894d093a785be6823abd
Vladimir RM <shvdrummer@gmail.com>
2023-03-15 07:53:43 +0200

    find  num with push done

--- index.js
+++ index.js
@@ -18,11 +18,11 @@ let arr = [1, 2, 3, 4, 5, 6, 7];
 
 // let arr = [];
 
-let sum = 0;
+let sum = [];
 
 for (let i = 0; i < arr.length; i++) {
   if (arr[i] === 4) {
-    sum = arr[i];
+    sum.push(arr[i]);
   }
 }
 
