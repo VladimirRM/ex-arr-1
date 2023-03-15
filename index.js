1a2320ccd7f7f1b12336774da7878022bfe57ada
Vladimir RM <shvdrummer@gmail.com>
2023-03-15 08:00:56 +0200

    max with Math done

--- index.js
+++ index.js
@@ -21,9 +21,7 @@ let arr = [1, 2, 3, 4, 5, 6, 7];
 let sum = [];
 
 for (let i = 0; i < arr.length; i++) {
-  if (arr[i] > sum) {
-    sum = arr[i];
-  }
+  sum = Math.max(arr[i], sum);
 }
 
 console.log(sum);
