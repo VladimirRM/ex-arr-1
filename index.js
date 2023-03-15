aeb5c5abe7ad37aa70f141d895c0a9828dd6a8ac
Vladimir RM <shvdrummer@gmail.com>
2023-03-15 07:52:31 +0200

    find number in arr with for done

--- index.js
+++ index.js
@@ -21,10 +21,11 @@ let arr = [1, 2, 3, 4, 5, 6, 7];
 let sum = 0;
 
 for (let i = 0; i < arr.length; i++) {
-  sum += arr[i];
-  result = sum / arr.length;
+  if (arr[i] === 4) {
+    sum = arr[i];
+  }
 }
 
-console.log(result);
+console.log(sum);
 
 // for( let i = 0; i<arr.length; i++){}
