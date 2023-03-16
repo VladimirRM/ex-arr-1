06927193ddda89696fe71db591f6485bc6e254b4
Vladimir RM <shvdrummer@gmail.com>
2023-03-16 07:38:29 +0200

    pushing 20 random number done

--- index.js
+++ index.js
@@ -12,16 +12,16 @@
 
 // const arr = [7, 0, "hello", 5, false, NaN];
 
-let arr = [1, 2, -2, 2, 3, -4, 5, -5, 5, 5, 6, 7];
+// let arr = [1, 2, -2, 2, 3, -4, 5, -5, 5, 5, 6, 7];
 
 // let arr = [1, 2, 3, 4, 5, 6, 7];
 
-let sum = arr[0];
+let arr = [];
 
-for (let i = 0; i < arr.length; i++) {
-  sum = Math.max(arr[i], sum);
+for (let i = 0; i < 20; i++) {
+  arr.push(Math.round(Math.random() * 20) + 1);
 }
 
-console.log(sum);
+console.log(arr);
 
 // for( let i = 0; i<arr.length; i++){}
