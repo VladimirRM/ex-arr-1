2a901525bbc06dba4422c6701bdd7fb6b208ce27
Vladimir RM <shvdrummer@gmail.com>
2023-03-16 07:42:31 +0200

    Sort arr number done

--- index.js
+++ index.js
@@ -16,11 +16,8 @@
 
 let arr = [1, 2, 3, 4, 5, 6, 7];
 
-let sum = 0;
+arr.sort((a, b) => a - b);
 
-for (let i = 0; i < arr.length; i++) {
-  sum += arr[i] * arr[i];
-}
-console.log(sum);
+console.log(arr);
 
 // for( let i = 0; i<arr.length; i++){}
