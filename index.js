8276dbd0b045f902e829c0df315530f1c12e8b75
Vladimir RM <shvdrummer@gmail.com>
2023-03-17 08:30:35 +0200

    Sum using for done

--- index.js
+++ index.js
@@ -10,20 +10,18 @@
 // 10 reverse num, for, map;
 // 11 false arr for, filter
 
-const arr = [7, 0, "hello", 5, false, NaN];
+// const arr = [7, 0, "hello", 5, false, NaN];
 
-// let arr = [1, 2, -2, 2, 3, -4, 5, -5, 5, 5, 6, 7];
+let arr = [1, 2, -2, 2, 3, -4, 5, -5, 5, 5, 6, 7];
 
 // let arr = [1, 2, 3, 4, 5, 6, 7];
 
-let res = [];
+let sum = 0;
 
 for (let i = 0; i < arr.length; i++) {
-  if (!!arr[i]) {
-    res.push(arr[i]);
-  }
+  sum += arr[i];
 }
 
-console.log(res);
+console.log(sum);
 
 // for( let i = 0; i<arr.length; i++){}
