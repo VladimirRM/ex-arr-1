1c762055a958f6f99857e606fed8586c979b35da
Vladimir RM <shvdrummer@gmail.com>
2023-03-17 08:46:43 +0200

    Done ex

--- index.js
+++ index.js
@@ -18,10 +18,5 @@ let arr = [1, 2, 3, 4, 5, 6, 7];
 
 let sum = 0;
 
-for (let i = 0; i < arr.length; i++) {
-  sum += arr[i] * arr[i];
-}
-
-console.log(sum);
 
 // for( let i = 0; i<arr.length; i++){}
