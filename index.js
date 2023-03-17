dac876a5b5e9e2c486e8f44af33274f2bc6dc185
Vladimir RM <shvdrummer@gmail.com>
2023-03-17 08:42:23 +0200

    Max num using reduce done

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ let arr = [1, 2, -2, 2, 3, -4, 5, -5, 5, 5, 6, 7];
 
 // let arr = [1, 2, 3, 4, 5, 6, 7];
 
-let sum = arr[0];
+let sum = arr.reduce((a, b) => Math.max(a,b));
 
-for (let i = 0; i < arr.length; i++) {
-  if (arr[i] > sum) {
-    sum =arr[i];
-  }
-}
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] > sum) {
+//     sum =arr[i];
+//   }
+// }
 
 console.log(sum);
 
