87121c637f4d85c7cfc90fab915c62be13896791
Vladimir RM <shvdrummer@gmail.com>
2023-03-18 10:16:15 +0200

    Get num done

--- index.js
+++ index.js
@@ -19,9 +19,10 @@ let arr = [1, 2, 3, 4, 5, 6, 7];
 let sum = 0;
 
 for (let i = 0; i < arr.length; i++) {
-  sum += arr[i];
-  res = sum / arr.length;
+  if (arr[i] === 2) {
+    sum = arr[i];
+  }
 }
-console.log(res);
+console.log(sum);
 
 // for( let i = 0; i<arr.length; i++){}
