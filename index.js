7a8858f5454cd8ce2d1794668fa261a5794b42f1
Vladimir RM <shvdrummer@gmail.com>
2023-03-19 08:52:26 +0200

    Find num in arr done

--- index.js
+++ index.js
@@ -16,13 +16,14 @@
 
 let arr = [1, 2, 3, 4, 5, 6, 7];
 
-let sum = 0;
+let sum = [];
 
 for (let i = 0; i < arr.length; i++) {
-  sum += arr[i];
-  res = sum / arr.length;
+  if (arr[i] === 2) {
+    sum.push(arr[i]);
+  }
 }
 
-console.log(res);
+console.log(sum);
 
 // for( let i = 0; i<arr.length; i++){}
