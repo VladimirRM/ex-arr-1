57490350fe5ed340ffb7745854620340e34acc33
Vladimir RM <shvdrummer@gmail.com>
2023-03-19 08:51:20 +0200

    Calc average using for

--- index.js
+++ index.js
@@ -20,8 +20,9 @@ let sum = 0;
 
 for (let i = 0; i < arr.length; i++) {
   sum += arr[i];
+  res = sum / arr.length;
 }
 
-console.log(sum);
+console.log(res);
 
 // for( let i = 0; i<arr.length; i++){}
