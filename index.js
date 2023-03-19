de76093d88bca22d1a1bfd52996602a0cc687305
Vladimir RM <shvdrummer@gmail.com>
2023-03-19 08:50:21 +0200

    Calc sum done

--- index.js
+++ index.js
@@ -16,13 +16,12 @@
 
 let arr = [1, 2, 3, 4, 5, 6, 7];
 
-let sum = [];
+let sum = 0;
 
 for (let i = 0; i < arr.length; i++) {
-  if (arr[i] > 0 && arr[i] < 4) {
-    sum.push(arr[i]);
-  }
+  sum += arr[i];
 }
+
 console.log(sum);
 
 // for( let i = 0; i<arr.length; i++){}
