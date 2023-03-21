d0726ac71969f77ee6a2f64f2e8efdb78cdfd0ce
Vladimir RM <shvdrummer@gmail.com>
2023-03-21 07:00:31 +0200

    Calc max number.

--- index.js
+++ index.js
@@ -15,10 +15,12 @@
 // let arr = [1, 2, -2, 2, 3, -4, 5, -5, 5, 5, 6, 7];
 
 let arr = [1, 2, 3, 4, 5, 6, 7];
-let sum = 0;
+let sum = arr[0];
 
 for (let i = 0; i < arr.length; i++) {
-  sum += arr[i] * arr[i];
+  if ((sum += arr[i])) {
+    sum = arr[i];
+  }
 }
 
 console.log(sum);
