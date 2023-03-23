97d951a4bada38005d42e0bc34f33368265fed4e
Vladimir RM <shvdrummer@gmail.com>
2023-03-23 09:23:20 +0200

    Push in arr random num.

--- index.js
+++ index.js
@@ -12,17 +12,16 @@
 
 // const arr = [7, 0, "hello", 5, false, NaN];
 
-let arr = [1, 2, -2, 2, 3, -4, 5, -5, 5, 5, 6, 7];
+// let arr = [1, 2, -2, 2, 3, -4, 5, -5, 5, 5, 6, 7];
 
-let sum = 0;
+let arr = [];
 
 // let arr = [1, 2, 3, 4, 5, 6, 7];
 
-for (let i = 0; i < arr.length; i++) {
-  sum += arr[i];
-  result = sum / arr.length;
+for (let i = 0; i < 20; i++) {
+  arr.push(Math.round(Math.random() * 20) + 1);
 }
 
-console.log(result);
+console.log(arr);
 
 // for( let i = 0; i<arr.length; i++){}
