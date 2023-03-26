055997a0e2860697627828d66f5202d97a50810f
Vladimir RM <shvdrummer@gmail.com>
2023-03-26 09:59:25 +0300

    Get number in arr.

--- index.js
+++ index.js
@@ -20,10 +20,11 @@ let res = 0;
 
 let arr = [1, 2, 3, 4, 5, 6, 7];
 for (let i = 0; i < arr.length; i++) {
-  res += arr[i];
-  sum = res / arr.length;
+  if (arr[i] === 2) {
+    res = arr[i];
+  }
 }
 
-console.log(sum);
+console.log(res);
 
 // for( let i = 0; i<arr.length; i++){}
