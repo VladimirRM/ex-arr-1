146f4c9665bcf799f7a71f46456647f2884b6995
Vladimir RM <shvdrummer@gmail.com>
2023-03-26 10:40:37 +0300

    Sort arr age.

--- index.js
+++ index.js
@@ -10,14 +10,21 @@
 // 10 reverse num, for, map;
 // 11 false arr for, filter
 
-const arr = [7, 0, "hello", 5, false, NaN];
+user = [
+  { name: "ana", age: 32 },
+  { name: "ivan", age: 33 },
+  { name: "petro", age: 34 },
+  { name: "kuzya", age: 60 },
+];
+
+// const arr = [7, 0, "hello", 5, false, NaN];
 
 // let arr = [1, 2, -2, 2, 3, -4, 5, -5, 5, 5, 6, 7];
 
-let res = arr.filter(Boolean);
+user.sort((a, b) => a - b);
 
 // let arr = [1, 2, 3, 4, 5, 6, 7];
 
-console.log(res);
+console.log(user);
 
 // for( let i = 0; i<arr.length; i++){}
