1ea17892c7ca2d9ab377f26bcaa9ccb1ed0a14a4
Vladimir RM <shvdrummer@gmail.com>
2023-03-26 10:03:55 +0300

    Delete duplicate in arr

--- index.js
+++ index.js
@@ -12,19 +12,20 @@
 
 // const arr = [7, 0, "hello", 5, false, NaN];
 
-// let arr = [1, 2, -2, 2, 3, -4, 5, -5, 5, 5, 6, 7];
+let arr = [1, 2, -2, 2, 3, -4, 5, -5, 5, 5, 6, 7];
 
 // let arr = [];
 
-let res = 0;
+// let res = 0;
 
-let arr = [1, 2, 3, 4, 5, 6, 7];
+// let arr = [1, 2, 3, 4, 5, 6, 7];
 for (let i = 0; i < arr.length; i++) {
-  if (arr[i] === 2) {
-    res = arr[i];
+  if (arr[i] === arr[i - 1]) {
+    arr.splice(i, 1);
+    i--;
   }
 }
 
-console.log(res);
+console.log(arr);
 
 // for( let i = 0; i<arr.length; i++){}
