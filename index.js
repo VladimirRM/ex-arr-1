38b7d308bb2a84533add4299e1183e9c0f645039
Vladimir RM <shvdrummer@gmail.com>
2023-03-26 10:11:50 +0300

    Add 20 random num in arr.

--- index.js
+++ index.js
@@ -12,19 +12,15 @@
 
 // const arr = [7, 0, "hello", 5, false, NaN];
 
-let arr = [1, 2, -2, 2, 3, -4, 5, -5, 5, 5, 6, 7];
+// let arr = [1, 2, -2, 2, 3, -4, 5, -5, 5, 5, 6, 7];
 
-// let arr = [];
-
-let res = arr.reduce((a, b) => Math.max(a,b));
+let arr = [];
 
 // let arr = [1, 2, 3, 4, 5, 6, 7];
-// for (let i = 0; i < arr.length; i++) {
-//   if (arr[i] > res) {
-//     res = arr[i];
-//   }
-// }
+for (let i = 0; i < 20; i++) {
+  arr.push(Math.round(Math.random() * 20) + 1);
+}
 
-console.log(res);
+console.log(arr);
 
 // for( let i = 0; i<arr.length; i++){}
