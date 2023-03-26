5e6a89dfd9c635417f286427498418df07a5592d
Vladimir RM <shvdrummer@gmail.com>
2023-03-26 10:06:50 +0300

    Calc max num using for.

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ let arr = [1, 2, -2, 2, 3, -4, 5, -5, 5, 5, 6, 7];
 
 // let arr = [];
 
-let res = [];
+let res = [0];
 
 // let arr = [1, 2, 3, 4, 5, 6, 7];
 for (let i = 0; i < arr.length; i++) {
-  if (arr[i] > 0 && arr[i] < 4) {
-    res.push(arr[i]);
+  if (arr[i] > res) {
+    res = arr[i];
   }
 }
 
