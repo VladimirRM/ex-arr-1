93575c6716747ecac303a37f1360bfd48fc42ab7
Vladimir RM <shvdrummer@gmail.com>
2023-03-31 14:55:20 +0300

    Get sum of array nums.

--- index.js
+++ index.js
@@ -10,21 +10,24 @@
 // 10 reverse num, for, map;
 // 11 false arr for, filter
 
-user = [
-  { name: "ana", age: 32 },
-  { name: "ivan", age: 33 },
-  { name: "petro", age: 34 },
-  { name: "kuzya", age: 60 },
-];
+// user = [
+//   { name: "ana", age: 32 },
+//   { name: "ivan", age: 33 },
+//   { name: "petro", age: 34 },
+//   { name: "kuzya", age: 60 },
+// ];
 
 // const arr = [7, 0, "hello", 5, false, NaN];
 
-// let arr = [1, 2, -2, 2, 3, -4, 5, -5, 5, 5, 6, 7];
-
-user.sort((a, b) => a - b);
+let arr = [1, 2, -2, 2, 3, -4, 5, -5, 5, 5, 6, 7];
 
 // let arr = [1, 2, 3, 4, 5, 6, 7];
+let sum = 0;
 
-console.log(user);
+for (let i = 0; i < arr.length; i++) {
+  sum += arr[i];
+}
+
+console.log(sum);
 
 // for( let i = 0; i<arr.length; i++){}
