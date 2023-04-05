2b27ac8e0ee78f821747de2f976bf84affe7f52d
Vladimir RM <shvdrummer@gmail.com>
2023-04-05 08:35:54 +0300

    Get number in arr.

--- index.js
+++ index.js
@@ -23,14 +23,12 @@ let arr = [1, 2, -2, 2, 3, -4, 5, -5, 5, 5, 6, 7];
 
 // let arr = [1, 2, 3, 4, 5, 6, 7];
 let res = [];
-
 for (let i = 0; i < arr.length; i++) {
-  if (arr[i] === arr[i - 1]) {
-    arr.slice(i, 1);
-    i--;
+  if (arr[i] > 0 && arr[i] < 4) {
+    res.push(arr[i]);
   }
 }
 
-console.log(arr);
+console.log(res);
 
 // for( let i = 0; i<arr.length; i++){}
