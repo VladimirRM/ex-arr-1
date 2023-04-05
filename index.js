8f33ceef0b1bc904759635635e0ea3f6a9b97ca3
Vladimir RM <shvdrummer@gmail.com>
2023-04-05 08:37:18 +0300

    Calc max num in arr.

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ user = [
 let arr = [1, 2, -2, 2, 3, -4, 5, -5, 5, 5, 6, 7];
 
 // let arr = [1, 2, 3, 4, 5, 6, 7];
-let res = [];
+let res = arr[0];
 for (let i = 0; i < arr.length; i++) {
-  if (arr[i] > 0 && arr[i] < 4) {
-    res.push(arr[i]);
+  if (arr[i] > res) {
+    res = arr[i];
   }
 }
 
