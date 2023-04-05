a931087970944ce2677ff7b00e516d5f3ae4d894
Vladimir RM <shvdrummer@gmail.com>
2023-04-05 08:30:51 +0300

    Calc average in arr.

--- index.js
+++ index.js
@@ -26,8 +26,9 @@ let res = 0;
 
 for (let i = 0; i < arr.length; i++) {
   res += arr[i];
+  result = res / arr.length;
 }
 
-console.log(res);
+console.log(result);
 
 // for( let i = 0; i<arr.length; i++){}
