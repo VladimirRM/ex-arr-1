851bbd4cb1a0db96fa06a47947705a846edcaedc
Vladimir RM <shvdrummer@gmail.com>
2023-04-09 20:42:08 +0300

    PUsh in arr 20 random num done.

--- index.js
+++ index.js
@@ -19,12 +19,10 @@ user = [
 
 // const arr = [7, 0, "hello", 5, false, NaN];
 
-let arr = [1, 2, -2, 2, 3, -4, 5, -5, 5, 5, 6, 7];
+// let arr = [1, 2, -2, 2, 3, -4, 5, -5, 5, 5, 6, 7];
 let sum = [];
-for (let i = 0; i < arr.length; i++) {
-  if (arr[i] > 0 && arr[i] < 3) {
-    sum.push(arr[i]);
-  }
+for (let i = 0; i < 20; i++) {
+  sum.push(Math.round(Math.random() * 20) + 1);
 }
 console.log(sum);
 
